4c43d646db43e09272f3b1e82bf6bf83491d851d
andbrocode <andbro@tutanota.com>
2026-01-22 16:57:44 +0100

    updated examples; added plot_filtered_traces

--- sixdegrees/plots/__init__.py
+++ sixdegrees/plots/__init__.py
@@ -13,6 +13,7 @@ from .plot_velocities_win import plot_velocities_win
 from .plot_velocity_comparison import plot_velocity_comparison
 from .plot_velocity_method_comparison import plot_velocity_method_comparison
 from .plot_backazimuth_results import plot_backazimuth_results
+from .plot_filtered_traces_frequency_bands import plot_filtered_traces_frequency_bands
 
 __all__ = [
     'animate_waveforms', 
@@ -25,5 +26,6 @@ __all__ = [
     'plot_velocities_win',
     'plot_velocity_comparison',
     'plot_velocity_method_comparison',
-    'plot_backazimuth_results'
+    'plot_backazimuth_results',
+    'plot_filtered_traces_frequency_bands'
 ]
